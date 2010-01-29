9a1c77ea72f8e55f5599005b16f213826a0616e6
SamuraiJack <nickolay8@gmail.com>
2010-01-29 22:14:40 +0300

    - removed /lib & /static - 'static' renamed to 'resources'

--- build_from_svn.sh
+++ build_from_svn.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 rm -rf svnbuild/*
+rm -rf lib/*
+rm -rf static/*
 
-java -jar bin/JSBuilder2.jar --projectFile /home/catalyst/Workspace/EclipseWorkspace/JavaScript/ExtJS/ext.jsb2 --homeDir svnbuild
+java -jar bin/JSBuilder2.jar --projectFile /home/catalyst/Workspace/EclipseWorkspace/JavaScript/ExtJS-3.1.0/ext.jsb2 --homeDir svnbuild
 
+
+# mv -f svnbuild/ext-3.1+/ext-all.js
