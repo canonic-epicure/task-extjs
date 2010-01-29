ff3d2cff9e511b3735d309c99717ed8af1ce3da7
SamuraiJack <nickolay8@gmail.com>
2010-01-29 22:27:37 +0300

    - updated with 3.1.0 version

--- build_from_svn.sh
+++ build_from_svn.sh
@@ -2,9 +2,29 @@
 
 rm -rf svnbuild/*
 rm -rf lib/*
-rm -rf static/*
+rm -rf resources/*
 
 java -jar bin/JSBuilder2.jar --projectFile /home/catalyst/Workspace/EclipseWorkspace/JavaScript/ExtJS-3.1.0/ext.jsb2 --homeDir svnbuild
 
+mkdir -p lib/Task/ExtJS/Adapter
+mkdir -p lib/Task/ExtJS/Debug/Adapter
 
-# mv -f svnbuild/ext-3.1+/ext-all.js
+
+cp svnbuild/ext-3.1+/adapter/ext/ext-base.js                    lib/Task/ExtJS/Adapter/Ext.js
+cp svnbuild/ext-3.1+/adapter/ext/ext-base-debug.js              lib/Task/ExtJS/Debug/Adapter/Ext.js
+
+cp svnbuild/ext-3.1+/adapter/jquery/ext-jquery-adapter.js       lib/Task/ExtJS/Adapter/jQuery.js
+cp svnbuild/ext-3.1+/adapter/jquery/ext-jquery-adapter-debug.js lib/Task/ExtJS/Debug/Adapter/jQuery.js
+
+cp svnbuild/ext-3.1+/adapter/prototype/ext-prototype-adapter.js         lib/Task/ExtJS/Adapter/Prototype.js
+cp svnbuild/ext-3.1+/adapter/prototype/ext-prototype-adapter-debug.js   lib/Task/ExtJS/Debug/Adapter/Prototype.js
+
+cp svnbuild/ext-3.1+/adapter/yui/ext-yui-adapter.js                     lib/Task/ExtJS/Adapter/YUI.js
+cp svnbuild/ext-3.1+/adapter/yui/ext-yui-adapter-debug.js               lib/Task/ExtJS/Debug/Adapter/YUI.js
+
+
+cp svnbuild/ext-3.1+/ext-all.js                                 lib/Task/ExtJS/All.js
+cp svnbuild/ext-3.1+/ext-all-debug.js                           lib/Task/ExtJS/Debug/All.js
+
+
+cp -r svnbuild/ext-3.1+/resources resources
